c00a9d7ea9f13fe01202795182e3e55dcc60b400
pedago <pedago@pcalb-mm0417.univ-jfc.fr>
2019-09-25 13:30:44 +0200

    bowling final

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -6,11 +6,21 @@ package bowling;
  * final de ce joueur
  */
 public class SinglePlayerGame {
+    public int total;
+    public int cptStrike;
+    public int cptSpare;
+    public int cpt;
+    public int tour;
 
 	/**
 	 * Constructeur
 	 */
 	public SinglePlayerGame() {
+            this.total=0;
+            this.cpt=0;
+            this.cptSpare=0;
+            this.cptStrike=0;
+            this.tour=0;
 	}
 
 	/**
@@ -20,6 +30,57 @@ public class SinglePlayerGame {
 	 * ce lancé
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
+            this.tour+=1;
+            
+            if(this.tour>20){
+                if(this.cptStrike>2){
+                    this.total+=nombreDeQuillesAbattues;
+                    this.cptStrike-=1;
+                }
+                this.total+=nombreDeQuillesAbattues;
+                
+            }else{
+                if(this.cptStrike>0){
+                    if(this.cptStrike>2){
+                        this.total+=nombreDeQuillesAbattues*2;
+                        this.cptStrike-=2;
+                    }else{
+                        this.total+=nombreDeQuillesAbattues;
+                        this.cptStrike-=1;
+                    }
+                }
+                if (this.cptSpare>0){
+                    this.total+=nombreDeQuillesAbattues;
+                    this.cptSpare-=1;
+                }
+                if(this.tour%2!=0){
+                    if(nombreDeQuillesAbattues==10){
+                        ///STRIKE///
+                        this.total+=10;
+                        this.cptStrike+=2;
+                        this.tour+=1;
+                    }else{
+                        this.cpt+=nombreDeQuillesAbattues;
+                        
+                    }
+                    
+                }else{
+                    if(this.cpt+nombreDeQuillesAbattues==10){
+                        ///SPARE///
+                        this.total+=this.cpt+nombreDeQuillesAbattues;
+                        this.cpt=0;
+                        this.cptSpare+=1;
+                    }else{
+                        this.cpt+=nombreDeQuillesAbattues;
+                        this.total+=this.cpt;
+                        this.cpt=0;
+                    }
+                }
+                
+                
+                
+                
+            }
 
 	}
 
@@ -29,6 +90,7 @@ public class SinglePlayerGame {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+            return this.total;
+		//throw new UnsupportedOperationException("Pas encore implémenté");
 	}
 }
